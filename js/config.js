9b8dd4550611b4810372fcaedb7965d642ec6b92
ibo1337f <75735730+ibo1337f@users.noreply.github.com>
2021-11-15 09:10:46 +0300

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,53 @@
+/* 
+  Конифг для твоего сайта
+  Можешь менять данные
+*/
+
+let config = {
+
+  // Задержка загрузки (1 секунда)
+  preloaderDelay: 2.5,
+
+  // Задержка появления видео (4.2 секунды)
+  showDelay: 5.0, 
+
+  // Заголовок
+  title: 'ghtxfsh',
+
+  // Социальные сети
+  social: {
+    telegram: '@ghtxfsh',
+    steam: 'iditenaxui',
+    vk: 'ghtxfsh',
+  },
+
+
+
+
+
+  // Цвет иконки загрузки
+  preloaderColor: '#00008B',
+
+
+  // Цвет кнопки play
+  playButtonColor: '#ff0000',
+  // Цвет тени кнопки play
+  playButtonShadowColor: '#313036',
+
+
+  // Цвет ee00ff
+  titleColor: '#ff0000',
+  // Цвет тени загаловка
+  titleShadowColor: '#313036',
+
+
+  // Текст копирайта
+  copyrightText: '6six6six6six',
+  // Цвет копирайта
+  copyrightTextColor: '#ff0000',
+
+  // Цвет иконок социальных сетей
+  socialColor: '#ff0000',
+  // Цвет тени иконок социальных сетей
+  socialShadowColor: '#313036',
+}
